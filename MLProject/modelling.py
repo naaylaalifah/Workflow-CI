2178787eab9b4cee806845291eba0459519afb7a
Nayla <naaylaalifah@gmail.com>
2025-12-24 22:47:09 +0700

    update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -6,8 +6,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
-mlflow.set_experiment("CI_AirQuality_Model")
-
 df = pd.read_csv("preprocessed_data.csv")
 
 X = df.drop(columns=["C6H6(GT)"])
@@ -17,30 +15,29 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-with mlflow.start_run():
-    model = RandomForestRegressor(
-        n_estimators=100,
-        random_state=42
-    )
+model = RandomForestRegressor(
+    n_estimators=100,
+    random_state=42
+)
 
-    model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
 
-    rmse = mean_squared_error(y_test, y_pred) ** 0.5
-    mae = mean_absolute_error(y_test, y_pred)
-    r2 = r2_score(y_test, y_pred)
+rmse = mean_squared_error(y_test, y_pred) ** 0.5
+mae = mean_absolute_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
 
-    mlflow.log_param("n_estimators", 100)
-    mlflow.log_metric("rmse", rmse)
-    mlflow.log_metric("mae", mae)
-    mlflow.log_metric("r2_score", r2)
+mlflow.log_param("n_estimators", 100)
+mlflow.log_metric("rmse", rmse)
+mlflow.log_metric("mae", mae)
+mlflow.log_metric("r2_score", r2)
 
-    mlflow.sklearn.log_model(
-        model,
-        artifact_path="model",
-        input_example=X_test.iloc[:5]
-    )
+mlflow.sklearn.log_model(
+    model,
+    artifact_path="model",
+    input_example=X_test.iloc[:5]
+)
 
-    print("RMSE:", rmse)
-    print("MAE:", mae)
-    print("R2:", r2)
+print("RMSE:", rmse)
+print("MAE:", mae)
+print("R2:", r2)
